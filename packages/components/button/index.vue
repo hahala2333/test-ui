1194afb9c2a9646bde750c1c04210ffc24b4cef7
dongruya <ruyad@ebtech.com>
2024-06-11 19:28:53 +0800

    开始创建组件阶段

---
+++ packages/components/button/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="my-button">测试按钮</button>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import "./style/index.less";
+export default defineComponent({
+  name: "my-button",
+  setup() {
+    return {};
+  },
+});
+</script>
+<style lang="less" scoped>
+@import "./style/index.less";
+</style>
